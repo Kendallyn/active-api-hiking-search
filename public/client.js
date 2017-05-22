0f0d9659383f29283436ecafee36b2bb26ef07aa
Kendallyn <Kendallynlyon@hotmail.com>
2017-05-22 13:40:19 -0700

    search reults function added

--- public/client.js
+++ public/client.js
@@ -1,1 +1,8 @@
-//Use input from user and display external API results in webpage
+//Use input from user and display external API results in webpage, can use console.log for now
+"use strict"
+
+$("#search").submit(function (event) {
+    event.preventDefault();
+    var userSearch = $("#searchField").val();
+    getSearchResults(userSearch);
+});
