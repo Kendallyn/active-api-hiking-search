b7d9af3a4bd50cf30e1493a969e67449c9c8a124
Kendallyn <Kendallynlyon@hotmail.com>
2017-05-28 16:45:02 -0700

    added start and end server functions

--- test/test-active-api-hiking-search.js
+++ test/test-active-api-hiking-search.js
@@ -6,17 +6,72 @@ const chaiHttp = require('chai-http');
 const should = chai.should();
 chai.use(chaiHttp);
 
+const {
+    app, runServer, closeServer
+} = require('../server');
+
 var server = require('../server.js');
 var Activity = require('../models/activity.js');
 
-describe(Activity, function () {
-    it('should list activities on GET', function (done) {
-        chai.request(app)
-            .get('/activity/:location')
-            .then(function (err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
+describe('active-api-hiking-search', function () {
+    before(function (done) {
+        server.runServer(function () {
+            Activity.create({
+                name: 'Recreational Hike'
+            }, {
+                name: 'Outdoor Adventure'
+            }, {
+                name: 'Color Run'
+            }, function () {
                 done();
             });
+        });
+    });
+
+    describe(Activity, function () {
+        it('should list activities on GET', function (done) {
+            chai.request(app)
+                .get('/activity/:location')
+                .then(function (err, res) {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    done();
+                });
+        });
+
+        it('should add an activity on POST', function (done) {
+            chai.request(app)
+                .post('/add-to-favorites/')
+                .send({
+                    'name': 'Hiker'
+                })
+                .end(function (err, res) {
+                    should.equal(err, null);
+                    res.should.have.status(201);
+                    res.should.be.json;
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('name');
+                    res.body.should.have.property('_id');
+                    res.body.name.should.be.a('string');
+                    res.body._id.should.be.a('string');
+                    res.body.name.should.equal('Hiker');
+                    done();
+                });
+        });
+
+        is('should delete an item on DELETE', function (done) {
+            chai.request(app)
+                .delete('/')
+                .end(function (err, res) {
+                    res.should.have.status(404);
+                    done();
+                });
+        });
+    });
+
+    after(function (done) {
+        Activity.remove(function () {
+            done();
+        });
     });
 });
